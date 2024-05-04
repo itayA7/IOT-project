ff788c0ef6a5b4b98d46abe98a68914db1d63e2f
itayA7 <96634122+itayA7@users.noreply.github.com>
2024-05-04 16:12:48 +0300

    Update configFirebase.js
    removing fb data

--- node.js/configFirebase.js
+++ node.js/configFirebase.js
@@ -4,13 +4,13 @@ const { getDatabase } = require("firebase/database"); // Importing the getDataba
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpaInX5PJl8xVU_m8wmqvW4aOvqrBxuG0", // Firebase API key.
-  authDomain: "iot-final-june2023.firebaseapp.com", // Firebase authentication domain.
-  databaseURL: "https://iot-final-june2023-default-rtdb.europe-west1.firebasedatabase.app", // Firebase database URL.
-  projectId: "iot-final-june2023", // Firebase project ID.
-  storageBucket: "iot-final-june2023.appspot.com", // Firebase storage bucket.
-  messagingSenderId: "754993571905", // Firebase messaging sender ID.
-  appId: "1:754993571905:web:3b45b0b1a94a10e0187ec0", // Firebase app ID.
+  apiKey: "", // Firebase API key.
+  authDomain: "", // Firebase authentication domain.
+  databaseURL: "", // Firebase database URL.
+  projectId: "", // Firebase project ID.
+  storageBucket: "", // Firebase storage bucket.
+  messagingSenderId: "", // Firebase messaging sender ID.
+  appId: "", // Firebase app ID.
 };
 
 // Initialize Firebase
